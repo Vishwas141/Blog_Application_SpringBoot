c0762b31f74883588f740ddf7c4aa8c05761ff75
vishwas <nalawadevishwas96@gmail.com>
2024-07-05 10:10:03 +0530

    trying to implemene spring security

---
+++ src/main/java/com/BlogApplication/Blogging/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.BlogApplication.Blogging.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfiguration {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests();
+
+
+    }
+
+}
