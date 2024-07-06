76b55f40100e344e2cb392c9915c6632bd77c5fa
vishwas <nalawadevishwas14@gmail.com>
2024-07-06 19:15:23 +0530

    Comment Entity created

---
+++ src/main/java/com/BlogApplication/Blogging/Entity/Comment.java
@@ -0,0 +1,16 @@
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// This class is used to create a comment entity.
+
+@Entity
+public class comment()
+{
+    private String Content;
+    private String Author;
+    private String Date;
+
+};
